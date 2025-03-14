9e6fae565449c8b7cc24a3fc5701b1aa86f64ae0
“leejiyun1” <poi20701556@gmail.com>
2025-03-14 14:40:01 +0900

    ✅ 7일차 과제 제출
    과제1~9 제출합니다.

---
+++ 7일차/animals/birds.py
@@ -0,0 +1,7 @@
+class Eagle:
+    def __init__(self, name):
+        self.name = name
+
+
+    def introduce(self):
+        return "난 독수리다."
---
+++ 7일차/animals/mammals.py
@@ -0,0 +1,7 @@
+class Dog:
+    def __init__(self, name):
+        self.name = name
+
+
+    def introduce(self):
+        return "난 강아지다."
